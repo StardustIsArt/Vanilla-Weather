a11cb36656e24d5538268fcc970021d06b8b8372
StardustIsArt <yocius_sa@icloud.com>
2021-11-29 12:17:27 -0600

    added function displayTemperature

---
+++ src/script.js
@@ -0,0 +1,13 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+}
+let units = "metric";
+let city = "New York";
+let apiKey = "e1f92b0f0a53fe657eb50521404f459d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
