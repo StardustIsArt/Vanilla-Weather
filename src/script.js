a9c9590cdf323d8ddc9dbeeb0abf995540326dad
StardustIsArt <yocius_sa@icloud.com>
2021-11-29 14:01:25 -0600

    updated and added weather description

--- src/script.js
+++ src/script.js
@@ -1,9 +1,17 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
-  let temperatureElement = document.querySelector("#temperature");
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let feelsLikeElement = document.querySelector("#feelsLike");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  feelsLikeElement.innerHTML = `${Math.round(response.data.main.feels_like)}º`;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let units = "metric";
 let city = "New York";
