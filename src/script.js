46c8a4e96d176beeb4458872ddf39f5e5bbd3d28
StardustIsArt <yocius_sa@icloud.com>
2021-11-30 17:09:23 -0600

    Display forecast data using the API

--- src/script.js
+++ src/script.js
@@ -19,29 +19,54 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-      <div class="weather-forcast-date">${day}</div>
+      <div class="weather-forcast-date">${formatDay(forecastDay.dt)}</div>
+      
       <img
-        src="http://openweathermap.org/img/wn/04d@2x.png"
+        src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
         alt="cloudy"
         width="54"
       />
       <div class="weather-forcast-temperature">
-        <span class="weather-forcast-temperature-max">18º</span>
-        <span class="weather-forcast-temperature-min">12º</span>
+        <span class="weather-forcast-temperature-max">${Math.round(
+          forecastDay.temp.max
+        )}º</span>
+        <span class="weather-forcast-temperature-min">${Math.round(
+          forecastDay.temp.min
+        )}º</span>
       </div>
     </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
